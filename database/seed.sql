223d37be602bd4c89a29e492fdda2c851d5a8631
DeepsDali <deepashri.dali@gmail.com>
2023-09-26 14:59:19 +0100

    update see file

--- database/seed.sql
+++ database/seed.sql
@@ -1,22 +1,28 @@
+PRAGMA foreign_keys = OFF;
+
 BEGIN;
 
 INSERT INTO dogs VALUES
   (1,'Bruno', 'Cocker Spaniel', 1),
- (2,'Belinda', 'Pug', 2),
+  (2,'Belinda', 'Pug', 2),
   (3,'Piccasso', 'Cocker Spaniel', 2),
-   (4,'Luna', 'Rottweiler', 3),
-    (5,'Max', 'Dachshund', 4),
+  (4,'Luna', 'Rottweiler', 3),
+  (5,'Max', 'Dachshund', 4)
+ON CONFLICT DO NOTHING;
 
-    INSERT INTO owner VALUES
+INSERT INTO owners VALUES
   (1,'Deepa'),
- (2,'Dylan'),
+  (2,'Dylan'),
   (3,'Shaughn'),
-   (4,'Yuqing'),
-   
-    INSERT INTO booking VALUES
-  (1,2,'26Sep2023'),
- (2,3,'28Sep2023'),
-  (3,1,'29Sep2023'),
-   (4,5,'30Sep2023'),
-    (5,4,'30Sep2023'),
-  COMMIT;
+  (4,'Yuqing')
+ON CONFLICT DO NOTHING;   
+
+INSERT INTO bookings VALUES
+  (1,2,'2023-09-23'),
+  (2,3,'2023-09-24'),
+  (3,1,'2023-09-25'),
+  (4,5,'2023-09-28'),
+  (5,4,'2023-09-30')
+ON CONFLICT DO NOTHING;
+
+COMMIT;
