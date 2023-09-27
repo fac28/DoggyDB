63f04a04023e79318961a2d8b1c300ac08ca6f77
Hex <hexmusictheory@protonmail.com>
2023-09-27 15:33:07 +0100

    fix fly.io deployment being broken

--- Dockerfile
+++ Dockerfile
@@ -40,5 +40,5 @@ VOLUME /data
 
 # Start the server by default, this can be overwritten at runtime
 EXPOSE 3000
-ENV DATABASE_URL="file:///data/sqlite.db"
+ENV DB_FILE="/data/sqlite.db"
 CMD [ "npm", "start" ]
--- database/db.js
+++ database/db.js
@@ -1,16 +1,16 @@
-const { readFileSync } = require("node:fs");
-const { join } = require("node:path");
-const Database = require("better-sqlite3");
+const { readFileSync } = require('node:fs')
+const { join } = require('node:path')
+const Database = require('better-sqlite3')
 
-const db = new Database("db.sqlite");
-console.log(db);
+const db = new Database(process.env.DB_FILE)
+console.log(db)
 
-const schemaPath = join("database", "schema.sql");
-const schema = readFileSync(schemaPath, "utf-8");
-db.exec(schema);
+const schemaPath = join('database', 'schema.sql')
+const schema = readFileSync(schemaPath, 'utf-8')
+db.exec(schema)
 
-const select_table = db.prepare("SELECT name FROM sqlite_schema");
-const result = select_table.all();
-console.log(result);
+const selectTable = db.prepare('SELECT name FROM sqlite_schema')
+const result = selectTable.all()
+console.log(result)
 
-module.exports = db;
+module.exports = db
