81486802a46a183f99eddeb2b206c0bd0a5e300f
ShaughnAnderson94 <andersji94@gmail.com>
2023-09-27 15:14:29 +0100

    test does not add

--- test/serverPostTest.js
+++ test/serverPostTest.js
@@ -1,14 +1,27 @@
 const test = require("node:test");
 const assert = require("node:assert");
 const { request } = require("./helpers.js");
+const db = require("../database/db.js");
+
+const delete_test_booking = db.prepare(/*sql*/ `
+  DELETE FROM bookings WHERE date = ?
+`);
+
+function removeTestBooking() {
+  try {
+    delete_test_booking.run("1000-01-02");
+    console.log("Booking deleted successfully.");
+  } catch (error) {
+    console.error("Error deleting booking:", error);
+  }
+}
 
 test("form updates database", async () => {
-
   const requestBody = {
     dog_name: "Bruno",
-    date: "2024-01-02",
+    date: "1000-01-02",
     owner_name: "Deepa",
-    breed: "Cocker Spaniel"
+    breed: "Cocker Spaniel",
   };
 
   const post_response = await request("/book", {
@@ -23,7 +36,8 @@ test("form updates database", async () => {
   const { status, body } = await request("/");
   assert.match(
     body,
-    /<td>2024-01-02<\/td>/i,
-    `Expected HTML to include "<td>2024-01-02</td>", but received:\n${body}`
+    /<td>1000-01-02<\/td>/i,
+    `Expected HTML to include "<td>1000-01-02</td>", but received:\n${body}`
   );
+  removeTestBooking();
 });
