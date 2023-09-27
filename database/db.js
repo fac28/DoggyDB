31481013b19173c1399cb7728040aa877a3d3dca
Hex <hexmusictheory@protonmail.com>
2023-09-27 16:27:06 +0100

    add fallback to local db file if no env var provided

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const { readFileSync } = require('node:fs')
 const { join } = require('node:path')
 const Database = require('better-sqlite3')
 
-const db = new Database(process.env.DB_FILE)
+const db = new Database(process.env.DB_FILE || 'db.sqlite')
 console.log(db)
 
 const schemaPath = join('database', 'schema.sql')
