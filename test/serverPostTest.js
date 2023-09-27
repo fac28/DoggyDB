d2bc10fcdc61e1a2da5dfbe36bcbe8ae6b0679cb
ShaughnAnderson94 <andersji94@gmail.com>
2023-09-27 13:55:20 +0100

    tests  url updated

--- test/serverPostTest.js
+++ test/serverPostTest.js
@@ -3,7 +3,7 @@ const assert = require("node:assert");
 const { request } = require("./helpers.js");
 
 test("form updates database", async () => {
-  const post_response = await request("/form", {
+  const post_response = await request("/book", {
     method: "POST",
     body: "owner_name=Deepa&dog_name=Bruno&breed=Cocker Spaniel&date=2024-01-02",
     headers: { "content-type": "application/x-www-form-urlencoded" },
