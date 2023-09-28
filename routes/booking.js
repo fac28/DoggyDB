a35388b298f7894597e1ed63d21a8eb0710f9180
ShaughnAnderson94 <andersji94@gmail.com>
2023-09-28 12:18:06 +0100

    button fixes

--- routes/booking.js
+++ routes/booking.js
@@ -1,42 +1,47 @@
-const { Layout, Table } = require('../templates.js')
-const { listBookings } = require('../model/bookings.js')
+const { Layout, Table } = require("../templates.js");
+const { listBookings } = require("../model/bookings.js");
 
-function get (req, res) {
+function get(req, res) {
   // Parse query parameters for sorting
-  const sortColumn = req.query.sort || 'date' // Default to sorting by 'date'
-  const sortOrder = req.query.order || 'asc' // Default to ascending order
+  const sortColumn = req.query.sort || "date"; // Default to sorting by 'date'
+  const sortOrder = req.query.order || "asc"; // Default to ascending order
 
-  const bookings = listBookings()
+  const bookings = listBookings();
 
-  function sortBookings (bookings) {
+  function sortBookings(bookings) {
     bookings.sort((a, b) => {
-      const aValue = a[sortColumn]
-      const bValue = b[sortColumn]
+      const aValue = a[sortColumn];
+      const bValue = b[sortColumn];
 
-      if (sortOrder === 'asc') {
-        return aValue.localeCompare(bValue)
+      if (sortOrder === "asc") {
+        return aValue.localeCompare(bValue);
       } else {
-        return bValue.localeCompare(aValue)
+        return bValue.localeCompare(aValue);
       }
-    })
+    });
   }
 
   if (bookings.length === 0) {
-    const title = 'No bookings found'
-    const content = '<h1>No bookings found</h1>'
-    const body = Layout({ title, content })
-    return res.send(body)
+    const title = "No bookings found";
+    const content = "<h1>No bookings found</h1>";
+    const body = Layout({ title, content });
+    return res.send(body);
   }
 
-  const title = 'Bookings'
-  sortBookings(bookings)
-  let content = Table({ caption: title, data: bookings, sortColumn, sortOrder })
+  const title = "Bookings";
+  sortBookings(bookings);
+  let content = Table({
+    caption: title,
+    data: bookings,
+    sortColumn,
+    sortOrder,
+  });
 
-  content = /* html */`${content} <a href="/book"><button>Add booking</button></a>
-  <a href="/register"><button>Join the club</button></a>`
+  content = /* html */ `${content} <a href="/book" id='bookbutt'><button>Add booking</button></a>
+  `;
 
-  const body = Layout({ title, content })
-  res.send(body)
+  const body = Layout({ title, content });
+  res.send(body);
 }
 
-module.exports = { get }
+module.exports = { get };
