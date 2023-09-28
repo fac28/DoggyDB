cad315eb6eefad7dc9ded6a1f7a7c451e408f614
Yuqing Wang <bibolikepeaches@gmail.com>
2023-09-28 10:51:54 +0100

    modified based on live code feedback

--- routes/booking.js
+++ routes/booking.js
@@ -2,36 +2,37 @@ const { Layout, Table } = require('../templates.js')
 const { listBookings } = require('../model/bookings.js')
 
 function get (req, res) {
-  let title = 'Appointments'
-  const bookings = listBookings()
-  let content = '<h1>No bookings found</h1>'
-
   // Parse query parameters for sorting
-  const sortColumn = req.query.sort || 'date' // Default to sorting by 'id'
+  const sortColumn = req.query.sort || 'date' // Default to sorting by 'date'
   const sortOrder = req.query.order || 'asc' // Default to ascending order
 
-  // Sort the bookings array based on the selected column and order
-  bookings.sort((a, b) => {
-    const aValue = a[sortColumn]
-    const bValue = b[sortColumn]
+  const bookings = listBookings()
 
-    if (sortOrder === 'asc') {
-      return aValue.localeCompare(bValue)
-    } else {
-      return bValue.localeCompare(aValue)
-    }
-  })
+  function sortBookings (bookings) {
+    bookings.sort((a, b) => {
+      const aValue = a[sortColumn]
+      const bValue = b[sortColumn]
 
-  if (bookings.length === 0) {
-    res.status(404)
-    title = 'No bookings found'
-    content = '<h1>No bookings found</h1>'
-  } else {
-    title = 'Bookings'
-    content = Table({ caption: title, data: bookings, sortColumn, sortOrder })
+      if (sortOrder === 'asc') {
+        return aValue.localeCompare(bValue)
+      } else {
+        return bValue.localeCompare(aValue)
+      }
+    })
   }
 
-  content += /* html */`<a href="/book"><button>Add booking</button></a>
+  if (bookings.length === 0) {
+    const title = 'No bookings found'
+    const content = '<h1>No bookings found</h1>'
+    const body = Layout({ title, content })
+    return res.send(body)
+  }
+
+  const title = 'Bookings'
+  sortBookings(bookings)
+  let content = Table({ caption: title, data: bookings, sortColumn, sortOrder })
+
+  content = /* html */`${content} <a href="/book"><button>Add booking</button></a>
   <a href="/register"><button>Join the club</button></a>`
 
   const body = Layout({ title, content })
